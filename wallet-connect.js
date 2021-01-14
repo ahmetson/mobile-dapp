8f2d70d8fd0bcfa9f451da328f0f58967ab2991b
SchneiderNejc <nejc.sch@gmail.com>
2021-01-14 01:47:49 +0100

    updateVersion

--- wallet-connect.js
+++ wallet-connect.js
@@ -26,7 +26,7 @@ let accountContainer;
 
 
 
-alert('this is version 2.67');
+alert('this is version 2.7');
 /**
  * Setup the orchestra
  */
@@ -213,7 +213,7 @@ async function onConnect() {
 
     //Handle chain (network) and chainChanged (per EIP-1193)
     const chainId = await ethereum.request({ method: 'eth_chainId' });
-    handleChainChanged(chainId);
+    //handleChainChanged(chainId);
 
 
     //Handle user accounts and accountsChanged (per EIP-1193)
